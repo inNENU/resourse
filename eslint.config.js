419e5ce71d987b05c563ce7f91583558b2f2bcf6
Mister-Hope <mister-hope@outlook.com>
2024-05-06 15:21:52 +0800

    chore: update linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,13 @@
-// @ts-check
-import js from "@eslint/js";
-import eslintImport from "eslint-plugin-import";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import vitest from "eslint-plugin-vitest";
-import pluginVue from "eslint-plugin-vue";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import vueParser from "vue-eslint-parser";
+import hopeConfig, {
+  config,
+  globals,
+  tsParser,
+} from "eslint-config-mister-hope";
+import { vue, vueParser } from "eslint-config-mister-hope/vue";
 
-export default tseslint.config(
-  js.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  // @ts-expect-error: The type does not fit
-  ...pluginVue.configs["flat/recommended"],
+export default config(
+  ...vue,
+  ...hopeConfig,
   {
     ignores: [
       "**/node_modules/**",
@@ -22,96 +16,24 @@ export default tseslint.config(
       "**/.vuepress/.temp/",
     ],
   },
+
   {
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
       parser: vueParser,
       parserOptions: {
-        parser: tseslint.parser,
+        parser: tsParser,
         tsconfigDirName: import.meta.dirname,
-        project: ["./tsconfig.json"],
+        project: "./tsconfig.json",
         extraFileExtensions: [".vue"],
       },
     },
   },
-  {
-    files: ["**/*.js"],
-    ...tseslint.configs.disableTypeChecked,
-  },
-  {
-    plugins: {
-      import: eslintImport,
-    },
-    rules: {
-      curly: ["error", "multi", "consistent"],
-      "no-duplicate-imports": "off",
-      "no-unmodified-loop-condition": "error",
-      "padding-line-between-statements": [
-        "error",
-        {
-          blankLine: "always",
-          prev: ["const", "let"],
-          next: ["*"],
-        },
-        {
-          blankLine: "any",
-          prev: ["const", "let"],
-          next: ["const", "let"],
-        },
-        {
-          blankLine: "always",
-          prev: ["*"],
-          next: ["return"],
-        },
-      ],
-      "sort-imports": [
-        "error",
-        {
-          allowSeparatedGroups: false,
-          ignoreDeclarationSort: true,
-        },
-      ],
 
-      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
-      "import/first": "error",
-      "import/newline-after-import": "error",
-      "import/no-commonjs": "error",
-      "import/no-cycle": "error",
-      "import/no-duplicates": ["error", { considerQueryString: true }],
-      "import/no-named-default": "error",
-      "import/order": [
-        "error",
-        {
-          alphabetize: {
-            order: "asc",
-            orderImportKind: "asc",
-          },
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            ["parent", "sibling"],
-            "index",
-            "object",
-          ],
-          "newlines-between": "always",
-        },
-      ],
-    },
-  },
   {
     files: ["**/*.{ts,vue}"],
     rules: {
-      "@typescript-eslint/consistent-type-imports": "warn",
-      "@typescript-eslint/explicit-function-return-type": [
-        "warn",
-        {
-          allowHigherOrderFunctions: true,
-          allowDirectConstAssertionInArrowFunctions: true,
-          allowTypedFunctionExpressions: true,
-        },
-      ],
       "@typescript-eslint/naming-convention": [
         "warn",
         {
@@ -161,44 +83,6 @@ export default tseslint.config(
           format: ["PascalCase"],
         },
       ],
-      "@typescript-eslint/no-explicit-any": ["warn", { ignoreRestArgs: true }],
-      "@typescript-eslint/no-unsafe-member-access": "warn",
-      "@typescript-eslint/prefer-nullish-coalescing": "warn",
-      "vue/custom-event-name-casing": "error",
-      "vue/match-component-file-name": "error",
-      "vue/match-component-import-name": "error",
-      "vue/new-line-between-multi-line-property": "error",
-      "vue/next-tick-style": "error",
-      "vue/no-boolean-default": "warn",
-      "vue/no-duplicate-attr-inheritance": "error",
-      "vue/no-empty-component-block": "error",
-      "vue/no-multiple-objects-in-class": "error",
-      "vue/no-potential-component-option-typo": "error",
-      "vue/no-ref-object-destructure": "error",
-      "vue/no-required-prop-with-default": "error",
-      "vue/no-static-inline-styles": "error",
-      "vue/no-template-target-blank": "error",
-      "vue/no-this-in-before-route-enter": "error",
-      "vue/no-undef-components": "error",
-      "vue/no-undef-properties": "warn",
-      "vue/no-unsupported-features": "error",
-      "vue/no-unused-properties": "error",
-      "vue/no-unused-refs": "error",
-      "vue/no-useless-mustaches": "error",
-      "vue/no-useless-v-bind": "error",
-      "vue/padding-line-between-blocks": "error",
-      "vue/padding-line-between-tags": "error",
-      "vue/prefer-prop-type-boolean-first": "error",
-      "vue/prefer-separate-static-class": "error",
-      "vue/prefer-true-attribute-shorthand": "error",
-      "vue/require-direct-export": "error",
-      "vue/require-emit-validator": ["off"],
-      "vue/require-expose": "error",
-      "vue/require-name-property": "error",
-      "vue/require-prop-comment": "error",
-      "vue/script-indent": "error",
-      "vue/static-class-names-order": "error",
-      "vue/v-for-delimiter-style": "error",
     },
   },
   {
@@ -211,40 +95,13 @@ export default tseslint.config(
           style: { lang: "scss" },
         },
       ],
-      "vue/block-tag-newline": "error",
-      "vue/component-api-style": "error",
-      "vue/component-name-in-template-casing": ["error", "PascalCase"],
-      "vue/component-options-name-casing": "error",
-      "vue/component-tags-order": [
-        "error",
-        {
-          order: ["script", "template", "style"],
-        },
-      ],
-      "vue/define-emits-declaration": "error",
-      "vue/define-macros-order": "error",
-      "vue/define-props-declaration": "error",
-      "vue/html-button-has-type": "error",
-      "vue/html-comment-content-newline": "error",
-      "vue/html-comment-content-spacing": "error",
-      "vue/html-comment-indent": "error",
-      "vue/sort-keys": "error",
     },
   },
+
   {
     files: ["scripts/**.ts"],
     languageOptions: {
       globals: globals.node,
     },
   },
-  {
-    files: ["**/__tests__/**/*.spec.ts"],
-    plugins: {
-      vitest,
-    },
-    rules: {
-      ...vitest.configs.recommended.rules,
-    },
-  },
-  eslintPluginPrettierRecommended,
 );
