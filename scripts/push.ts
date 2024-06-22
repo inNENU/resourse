8ad6093c93eba222994ea834d4bed81407908318
Mr.Hope <mister-hope@outlook.com>
2024-06-23 03:18:28 +0800

    feat: update enroll

--- scripts/push.ts
+++ scripts/push.ts
@@ -25,6 +25,7 @@ export const pushPages = (): Promise<void> => {
       ? fetch(
           `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appIDInfo[appid]}`,
         )
+          // eslint-disable-next-line @typescript-eslint/naming-convention
           .then((res) => res.json() as Promise<{ access_token: string }>)
           .then((data) =>
             fetch(
--- scripts/utils/accessToken.ts
+++ scripts/utils/accessToken.ts
@@ -2,9 +2,9 @@ import { appIDInfo } from "../info.js";
 
 export const getWechatAccessToken = (appid: string): Promise<string> =>
   fetch(
-    // eslint-disable-next-line @typescript-eslint/naming-convention
     `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appIDInfo[appid]}`,
   )
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     .then((res) => res.json() as Promise<{ access_token: string }>)
     // eslint-disable-next-line @typescript-eslint/naming-convention
     .then(({ access_token }) => access_token);
